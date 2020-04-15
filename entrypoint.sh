81813392c319e0f1f7b79de6882aac0a070c1bb4
longkey1 <longkey1@gmail.com>
2020-04-15 12:00:08 +0900

    Fix useradd worker

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ GROUP_ID=${LOCAL_GID:-9001}
 
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
 mkdir -p /work
-useradd -u $USER_ID -o -d /work
+useradd -u $USER_ID -o -d /work worker
 groupmod -g $GROUP_ID worker
 export HOME=/work
 
