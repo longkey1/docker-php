3660fa9bafbe78252867947623a62df010a330a8
longkey1 <longkey1@gmail.com>
2020-04-16 11:21:19 +0900

    Clean code

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 USER_ID=${LOCAL_UID:-9001}
 GROUP_ID=${LOCAL_GID:-9001}
 
-echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+echo "Starting with UID : ${USER_ID}, GID: ${GROUP_ID}"
 useradd -u ${USER_ID} -o -d ${WORK_DIR} worker
 groupmod -g ${GROUP_ID} worker
 export HOME=/work
