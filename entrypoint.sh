1a7d9118ccc435f537ad9543208ddb6bfcceb9a7
longkey1 <longkey1@gmail.com>
2022-01-29 09:35:38 +0900

    refactor: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,13 @@ QUIET_MSG=${QUIET_MSG:-FALSE}
 
 if [ "${QUIET_MSG}" = "FALSE" ]; then
   echo "Starting with UID : ${USER_ID}, GID: ${GROUP_ID}"
-fi
-if [ "${QUIET_MSG}" = "FALSE" ]; then
   useradd -u ${USER_ID} -o -d ${WORK_DIR} worker
-else
-  useradd -u ${USER_ID} -o -d ${WORK_DIR} worker > /dev/null 2>&1
-fi
-if  [ "${QUIET_MSG}" = "FALSE" ]; then
   groupmod -g ${GROUP_ID} worker
 else
+  useradd -u ${USER_ID} -o -d ${WORK_DIR} worker > /dev/null 2>&1
   groupmod -g ${GROUP_ID} worker > /dev/null 2>&1
 fi
+
 export HOME=/work
 
 chown worker:worker ${WORK_DIR}
